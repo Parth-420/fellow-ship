01e825a46e0e5bebe26d086f555196ecf158966b
Parth-420 <parthg678@gmail.com>
2025-07-01 00:38:56 +0530

    change binding for render

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use axum::serve;
 #[tokio::main]
 async fn main() {
     let app = routes::create_router();
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("Listening on {}", addr);
     let listener = TcpListener::bind(addr).await.unwrap();
     serve(listener, app).await.unwrap();
